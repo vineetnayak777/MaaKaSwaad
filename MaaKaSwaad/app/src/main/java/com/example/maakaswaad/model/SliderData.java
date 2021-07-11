0f29cdfe3e419384ac6d395bd3d2aca931db29dc
Vineet Nayak S <vineethn.cs18@sahyadri.edu.in>
2021-07-11 13:49:32 +0530

    AutoMovingBar added

---
+++ MaaKaSwaad/app/src/main/java/com/example/maakaswaad/model/SliderData.java
@@ -0,0 +1,21 @@
+package com.example.maakaswaad.model;
+
+public class SliderData {
+    private Integer imgUrl;
+
+    // Constructor method.
+    public SliderData(Integer imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    // Getter method
+    public Integer getImgUrl() {
+        return imgUrl;
+    }
+
+    // Setter method
+    public void setImgUrl(Integer imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+}
